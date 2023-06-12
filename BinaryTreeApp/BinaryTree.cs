51609d0db070611ec9029b4d1f584e4349f413c9
AukeJacobs <52545966+AukeJacobs@users.noreply.github.com>
2023-06-13 00:24:49 +0200

    Add project files.

---
+++ BinaryTreeApp/BinaryTree.cs
@@ -0,0 +1,54 @@
+﻿namespace BinaryTreeApp
+{
+    public class BinaryTree<T> where T : IComparable<T>
+    {
+        public Node<T> Root { get; private set; } = null!;
+
+        public void Add(T value)
+        {
+            if (Root == null)
+            {
+                Root = new Node<T>(value);
+            }
+            else
+            {
+                Root.Add(value);
+            }
+        }
+
+        public static void PrintNodesWithOneChildAfter(Node<T> node)
+        {
+            if (node == null)
+                return;
+
+            if (IsNextNodeEndNode(node) && ((node.Left != null && node.Right == null) || (node.Left == null && node.Right != null)))
+            {
+                Console.WriteLine($"Found parent node with a single end note child: {node.Value}");
+            }
+                
+            PrintNodesWithOneChildAfter(node.Left);
+            PrintNodesWithOneChildAfter(node.Right);
+        }
+
+        private static bool IsNextNodeEndNode(Node<T> node)
+        {
+            if (node.Left != null)
+            {
+                if (node.Left.Left == null && node.Left.Right == null && node.Right == null)
+                {
+                    return true;
+                }
+            }
+
+            if (node.Right != null)
+            {
+                if (node.Right.Left == null && node.Right.Right == null && node.Left == null)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ BinaryTreeApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using BinaryTreeApp;
+
+var tree = new BinaryTree<int>();
+
+foreach (var value in new[] { 50, 76, 17, 9, 23, 54, 14, 19, 72, 12, 67 })
+{
+    tree.Add(value);
+}
+
+BinaryTree<int>.PrintNodesWithOneChildAfter(tree.Root);
+    
